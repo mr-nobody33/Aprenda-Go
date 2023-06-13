eabdeacc9ca93638c39e8d66326cdd01bbcf7e0a
Helder Medeiros <helderberserk@gmail.com>
2023-06-12 21:22:46 -0300

    Ninja Nível #1 - Exercício 3

--- Ninja Nível #1/Exercício 3/Exercicio-3.go
+++ Ninja Nível #1/Exercício 3/Exercicio-3.go
@@ -1,9 +1,21 @@
 package main
 
-var x int
-var y string
-var z bool
+import "fmt"
+
+//- Utilizando a solução do exercício anterior:
+//1. Em package-level scope, atribua os seguintes valores às variáveis:
+//1. para "x" atribua 42
+//2. para "y" atribua "James Bond"
+//3. para "z" atribua true
+//2. Na função main:
+//1. Use fmt.Sprintf para atribuir todos esses valores a uma única variável. Faça essa atribuição de tipo string a uma variável de nome "s" utilizando o operador curto de declaração.
+//2. Demonstre a variável "s".
+
+var x int = 42
+var y string = "James Bond"
+var z bool = true
 
 func main() {
-	fmt.Printf("%v\n %v\n %v\n", x, y, z)
+	s := fmt.Sprintf("%v\t %v\t %v\t", x, y, z)
+	fmt.Printf(s)
 }

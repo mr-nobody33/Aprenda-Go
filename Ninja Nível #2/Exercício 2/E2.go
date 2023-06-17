289d6aacff6f6a0a4fdc045d225c104216aa2cb1
Helder Medeiros <helderberserk@gmail.com>
2023-06-17 15:05:29 -0300

    Nível #2 - Exercício 2

---
+++ Ninja Nível #2/Exercício 2/E2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Escreva expressões utilizando os operadores, e atribua seus valores a variáveis.
+// ==, !=, <=, <, >=, >
+// Demonstre estes valores.
+
+func main() {
+	a := (10 == 10)
+	b := (10 != 10)
+	c := (10 <= 10)
+	d := (10 < 10)
+	e := (10 >= 10)
+	f := (10 > 10)
+
+	fmt.Printf("%v\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e, f)
+}

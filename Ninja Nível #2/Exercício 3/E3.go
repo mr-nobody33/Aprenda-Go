4639e3c8a1dafe36b4b9688aa3979cc91d77e033
Helder Medeiros <helderberserk@gmail.com>
2023-06-17 15:24:13 -0300

    Nível #2 - Exercício 3

---
+++ Ninja Nível #2/Exercício 3/E3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Crie constantes tipadas e não-tipadas.
+// Demonstre seus valores.
+
+func main() {
+	const tipada int = 10
+	const naoTipada = 50
+
+	fmt.Println(tipada, naoTipada)
+}
